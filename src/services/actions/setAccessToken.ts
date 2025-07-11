3aa3a0dc3e1c8e51e40cfcda9933ef068995cf29
Zihad550 <dev.jehadhossain@gmail.com>
2025-07-12 05:37:55 +0600

    setup project

---
+++ src/services/actions/setAccessToken.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+import { authKey } from "@/contants/authkey";
+import { redirect } from "next/navigation";
+
+const setAccessToken = async (token: string, option?: any) => {
+  const cookieStore = await cookies();
+  cookieStore.set(authKey, token);
+  if (option && option.passwordChangeRequired) {
+    redirect("/dashboard/change-password");
+  }
+  if (option && !option.passwordChangeRequired && option.redirect) {
+    redirect(option.redirect);
+  }
+};
+
+export default setAccessToken;
