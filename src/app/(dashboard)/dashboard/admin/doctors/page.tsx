2d446668e24320997fdc8966bba602a1cdcd291c
Zihad550 <dev.jehadhossain@gmail.com>
2025-07-12 10:50:57 +0600

    feat: admin dashboard

---
+++ src/app/(dashboard)/dashboard/admin/doctors/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+import {
+  useDeleteDoctorMutation,
+  useGetAllDoctorsQuery,
+} from "@/redux/features/doctor/doctorApi";
+import { useDebounced } from "@/redux/hooks";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
+import DoctorModal from "./components/DoctorModal";
+
+const DoctorsPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const query: Record<string, any> = {};
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  // console.log(searchTerm);
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = searchTerm;
+  }
+
+  const { data, isLoading } = useGetAllDoctorsQuery({ ...query });
+  const [deleteDoctor] = useDeleteDoctorMutation();
+
+  // console.log(data);
+  const doctors = data?.doctors;
+  // console.log(doctors);
+
+  const handleDelete = async (id: string) => {
+    // console.log(id);
+    try {
+      const res = await deleteDoctor(id).unwrap();
+      // console.log(res);
+      if (res?.id) {
+        toast.success("Doctor deleted successfully!!!");
+      }
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "contactNumber", headerName: "Contact Number", flex: 1 },
+    { field: "gender", headerName: "Gender", flex: 1 },
+    { field: "apointmentFee", headerName: "Appointment Fee", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <IconButton
+              onClick={() => handleDelete(row.id)}
+              aria-label="delete"
+            >
+              <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+            <Link href={`/dashboard/admin/doctors/edit/${row.id}`}>
+              <IconButton aria-label="delete">
+                <EditIcon />
+              </IconButton>
+            </Link>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Button onClick={() => setIsModalOpen(true)}>Create New Doctor</Button>
+        <DoctorModal open={isModalOpen} setOpen={setIsModalOpen} />
+        <TextField
+          onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
+          placeholder="search doctors"
+        />
+      </Stack>
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={doctors} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
+    </Box>
+  );
+};
+
+export default DoctorsPage;
---
+++ src/app/(dashboard)/dashboard/admin/schedules/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Button, IconButton } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import ScheduleModal from "./components/ScheduleModal";
+
+import { useGetAllSchedulesQuery } from "@/redux/features/schedule/scheduleApi";
+import { ISchedule } from "@/types/schedule";
+import { dateFormatter } from "@/utils/dateFormatter";
+import dayjs from "dayjs";
+
+const SchedulesPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [allSchedule, setAllSchedule] = useState<any>([]);
+  const { data, isLoading } = useGetAllSchedulesQuery({});
+
+  const schedules = data?.schedules;
+
+  console.log(schedules);
+
+  useEffect(() => {
+    const updateData = schedules?.map((schedule: ISchedule, index: number) => {
+      return {
+        sl: index + 1,
+        id: schedule?.id,
+        startDate: dateFormatter(schedule.startDate),
+        endDate: dateFormatter(schedule.endDate),
+        startTime: dayjs(schedule?.startDate).format("hh:mm a"),
+        endTime: dayjs(schedule?.endDate).format("hh:mm a"),
+      };
+    });
+    setAllSchedule(updateData);
+  }, [schedules]);
+
+  const columns: GridColDef[] = [
+    { field: "sl", headerName: "SL" },
+    { field: "startDate", headerName: "Date", flex: 1 },
+    { field: "startTime", headerName: "Start Time", flex: 1 },
+    { field: "endTime", headerName: "End Time", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: () => {
+        return (
+          <IconButton aria-label="delete">
+            <DeleteIcon sx={{ color: "red" }} />
+          </IconButton>
+        );
+      },
+    },
+  ];
+  return (
+    <Box>
+      <Button onClick={() => setIsModalOpen(true)}>Create Schedule</Button>
+      <ScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
+      {!isLoading ? (
+        <Box my={2}>
+          <DataGrid rows={allSchedule ?? []} columns={columns} />
+        </Box>
+      ) : (
+        <h1>Loading.....</h1>
+      )}
+    </Box>
+  );
+};
+
+export default SchedulesPage;
